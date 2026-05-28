72633e2065ca833f16bd04aa03419b94a31b7b90
Marianna Miola <marianna.miola@cnr.it>
2026-05-28 15:42:55 +0200

    removed cache examples

--- examples/20_DGM_SMargherita/script/_old/01_test_MUSE_20_DGM_SMargherita.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export n=36
-
-for ((test=1; test<=${n}; test++))
-do
-    ./10_test_MUSE_20_DGM_SMargherita.sh project -d ../data/Temperatura_Ambiente_merge${test}.csv -p 20_DGM_SMargherita${test}
-done
